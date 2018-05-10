77d43b7cac2da54ce44b42bb6853b5451db07ab2
Sakshi Jain <Jain-Sakshi@users.noreply.github.com>
2018-05-10 09:56:12 +0530

    Preprocessing and query

---
+++ preprocessing.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  8 06:34:07 2018
+
+@author: logaprakash
+"""
+
+from collections import defaultdict
+from pyspark import SparkContext
+from pyspark.mllib.linalg import Vectors
+from pyspark.mllib.clustering import LDA
+from pyspark.sql import SQLContext
+import re
+import numpy as np
+import csv
+
+def document_vector(document):
+    id = document[1]
+    counts = defaultdict(int)
+    for token in document[0]:
+        if token in vocabulary:
+            token_id = vocabulary[token]
+            counts[token_id] += 1
+    counts = sorted(counts.items())
+    keys = [x[0] for x in counts]
+    values = [x[1] for x in counts]
+    return (id, Vectors.sparse(len(vocabulary), keys, values))
+
+
+sc = SparkContext('local', 'PySPARK LDA')
+sql_context = SQLContext(sc)
+
+data = sc.wholeTextFiles('/home/sakshi/Documents/big_data/data/*').map(lambda x: x[1])
+
+num_of_stop_words = 50      
+num_topics = 15	            
+num_words_per_topic = 100    
+max_iterations = 35         
+
+
+tokens = data                                                   \
+    .map( lambda document: document.strip().lower())            \
+    .map( lambda document: re.split("[\s;,#]", document))       \
+    .map( lambda word: [x for x in word if x.isalpha()])        \
+    .map( lambda word: [x for x in word if len(x) > 3] )
+
+
+termCounts = tokens                             \
+    .flatMap(lambda document: document)         \
+    .map(lambda word: (word, 1))                \
+    .reduceByKey( lambda x,y: x + y)            \
+    .map(lambda tuple: (tuple[1], tuple[0]))    \
+    .sortByKey(False)
+    
+threshold_value = termCounts.take(num_of_stop_words)[num_of_stop_words - 1][0]
+
+vocabulary = termCounts                         \
+    .filter(lambda x : x[0] < threshold_value)  \
+    .map(lambda x: x[1])                        \
+    .zipWithIndex()                             \
+    .collectAsMap()
+
+documents = tokens.zipWithIndex().map(document_vector).map(list)
+inv_voc = {value: key for (key, value) in vocabulary.items()}
+
+lda_model = LDA.train(documents, k=num_topics, maxIterations=max_iterations)
+topic_indices = lda_model.describeTopics(maxTermsPerTopic=num_words_per_topic)
+topic_document_matrix = lda_model.topicsMatrix()
+document_data_df = documents.map(lambda x: (x[0], x[1])).toDF(("DocID","Word_Counts"))
+word_data = document_data_df.select('Word_Counts').rdd.map(lambda x: x[0])   
+
+"""
+Vectorizing
+"""
+document_data_list = word_data.collect()
+num_docs = len(document_data_list)
+vocab_len = len(vocabulary)
+
+vectors = np.zeros((num_docs,num_topics))
+
+for document in range(0,num_docs):
+    for topic in range(0,num_topics):
+        for word in range(0,vocab_len):
+            if document_data_list[document][word] != 0.0:
+                vectors[document][topic] += document_data_list[document][word] * topic_document_matrix[word][topic]
+
+
+
+"""
+Saving required frames
+"""
+np.savetxt("/home/sakshi/Documents/big_data/vectors.csv", vectors, delimiter=",")
+np.savetxt("/home/sakshi/Documents/big_data/topic_word_matrix.csv", topic_document_matrix, delimiter=",")
+
+with open('/home/sakshi/Documents/big_data/vocabulary.csv', 'w') as f:  # Just use 'w' mode in 3.x
+    w = csv.DictWriter(f, vocabulary.keys())
+    w.writeheader()
+    w.writerow(vocabulary)
+
+"""
+Ending session
+"""
+sc.stop()
---
+++ query.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  9 19:21:22 2018
+
+@author: sakshi
+"""
+
+from collections import defaultdict
+from pyspark import SparkContext
+from pyspark.mllib.linalg import Vector, Vectors
+from pyspark.mllib.clustering import LDA, LDAModel
+from pyspark.sql import SQLContext
+import re
+import numpy as np
+import csv
+from time import time
+
+def document_vector(document):
+    id = document[1]
+    counts = defaultdict(int)
+    for token in document.split(' '):
+        if token in vocabulary:
+            token_id = vocabulary[token]
+            counts[token_id] += 1
+    counts = sorted(counts.items())
+    keys = [x[0] for x in counts]
+    values = [x[1] for x in counts]
+    
+    return (id, Vectors.sparse(len(vocabulary), keys, values))
+
+def getQueryVector(query):
+    query_word_count = document_vector(query.lower())
+    query_list = list(query_word_count)[1]
+    
+    query_vector = np.zeros((num_topics))
+    for topic in range(0,num_topics):
+        for word in range(0,vocab_len):
+            if query_list[word] != 0.0:
+                query_vector[topic] += query_list[word] * topic_word_matrix[word][topic]
+    
+    return query_vector
+
+def nMostSimilarDocuments(query_vector,no_of_results):
+    dist_matrix = np.zeros((num_docs,num_topics))
+    for dist_index in range(num_docs):
+        dist_matrix[dist_index] = vectors[dist_index] - query_vector.shape
+    
+    distances = np.sum(dist_matrix**2, axis=1)
+    return distances.argsort()[:no_of_results]
+
+def readFileLocations(path):
+    file_paths = []
+    files = glob.glob(path)
+    for element in files:
+        file_paths.append(element)
+    return file_paths
+
+    
+"""
+Reading model data
+"""
+dict_reader = csv.DictReader(open('/home/sakshi/Documents/big_data/vocabulary.csv', 'r'))
+vocabulary = []
+for line in dict_reader:
+    vocabulary.append(line)
+vocabulary = vocabulary[0]
+
+vectors = np.genfromtxt('/home/sakshi/Documents/big_data/vectors.csv', delimiter=',')
+topic_word_matrix = np.genfromtxt('/home/sakshi/Documents/big_data/topic_word_matrix.csv', delimiter=',')
+
+"""
+Declaring variables
+"""
+num_docs = vectors.shape[0]
+num_topics = topic_word_matrix.shape[1]
+vocab_len = len(vocabulary)
+no_of_results = 10
+individual_file_paths = get
+
+"""
+Main program
+"""
+query = input("Enter the query:\n")
+query_vec = getQueryVector(query)
+results = nMostSimilarDocuments(query_vec,no_of_results)
+print("Top " + str(no_of_results) + " documents for the query from the dataset are...\n")
+print(results)
+
+
+
+import glob
+import pandas as pd
+path = '/home/sakshi/Documents/big_data/data/*/*' #note C:
+files = glob.glob(path)
+files_data = []
+count = 0
+exceptions = 0
+names = []
+for name in files:
+    names.append(name)
+    try:
+        file = []
+        with open(name) as f:
+            for line in f:
+#                line.split('\n')
+                file.append(line)
+            files_data.append(file)
+    except:
+        print("Unknown exception")
+        exceptions+=1
+        files_data.append("File not found")
